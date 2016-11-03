b42890eb1a160065159e420e22a2cf60466f398a
Phillip Spicker <phillip.spicker@gmx.de>
2016-11-03 09:10:02 +0100

    adding and styling about, index and contact pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  get 'static_pages/about'
+
+  get 'static_pages/contact'
+
+  get 'static_pages/index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # You can have the root of your site routed with "root"
+  root 'static_pages#index'
 end
