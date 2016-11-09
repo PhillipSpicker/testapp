a205cf4ecf10cef0a3115ecfb20adcb0a3f6f87b
Phillip Spicker <phillip.spicker@gmx.de>
2016-11-09 14:37:48 +0100

    4.3 adding order controller and order routes

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   # You can have the root of your site routed with "root"
-  root 'static_pages#index'
+  root 'static_pages#landing_page'
 end
+
+resources :orders, only: [:index, :show, :create, :destroy]
