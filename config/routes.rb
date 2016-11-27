2f301b5d2d93c496325ef6167dbb752d1183b33b
Phillip Spicker <phillip.spicker@gmx.de>
2016-11-27 13:44:02 +0100

    5.1 setting routes and links

--- config/routes.rb
+++ config/routes.rb
@@ -6,10 +6,12 @@ Rails.application.routes.draw do
 
   get 'static_pages/index'
 
+  get 'static_pages/landing_page'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   # You can have the root of your site routed with "root"
-  root 'static_pages#landing_page'
+  root 'static_pages#index'
 
   resources :orders, only: [:index, :show, :create, :destroy]
 end
