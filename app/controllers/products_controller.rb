c7fbcef3d039421b3b42ac57d3df0bdac2d68d80
Phillip Spicker <phillip.spicker@gmx.de>
2016-12-01 08:20:18 +0100

    5.3 added email form and sendgrid

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -70,6 +70,20 @@ class ProductsController < ApplicationController
     end
   end
 
+  def thank_you
+    @name = params[:name]
+    @email = params[:email]
+    @message = params[:message]
+    ActionMailer::Base.mail(:from => @email,
+      :to => 'phillip.spicker@gmx.de',
+      :subject => "A new contact form message from #{@name}",
+      :body => @message).deliver_now
+  end
+
+
+
+
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_product
