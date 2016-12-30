6bd576e482caee950205c0445bb4b6757d52a963
Phillip Spicker <phillip.spicker@gmx.de>
2016-12-30 20:03:45 +0100

    change price from string to decimal

---
+++ db/migrate/20161230185958_change_price_to_decimal_in_products.rb
@@ -0,0 +1,11 @@
+class ChangePriceToDecimalInProducts < ActiveRecord::Migration[5.0]
+  def change
+  	def up
+    change_column :products, :price, :decimal
+  end
+
+  def down
+    change_column :products, :price, :string
+  end
+  end
+end
