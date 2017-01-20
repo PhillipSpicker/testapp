4cc7b1f3d7d76bef3e0f03d667b67d4c544104f5
Phillip Spicker <phillip.spicker@gmx.de>
2017-01-20 21:54:40 +0100

    6.10 add ajax and use on comments

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -1,15 +1,20 @@
 //file contains any site-wide js functions
-$(document).on('turbolinks:load', function(){
-    $('.rating').raty({ path: '/assets', scoreName: 'comment[rating]' });
-    $('.rated').raty({ path: '/assets',
-      readOnly: true,
-      score: function() {
-        return $(this).attr('data-score');
+var refreshRating = function() {
+  $('.rating').raty({ path: '/assets', scoreName: 'comment[rating]' });
+  $('.rated').raty({ path: '/assets',
+    readOnly: true,
+    score: function() {
+      return $(this).attr('data-score');
       }
     });
-});
+};
 
-$(document).on('turbolinks:load', function(){
+
+$(document).on('turbolinks:load ajaxSucess', function(){
+  $(".alert").delay(4000).fadeOut("fast");
+  /* raty plugin */
+  refreshRating();
+
   $('.img-zoom').elevateZoom({
     zoomType				: "lens",
     lensShape : "round",
