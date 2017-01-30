a40e428fae5a76910ccdfc288066d853079adf0c
Phillip Spicker <phillip.spicker@gmx.de>
2017-01-31 00:06:17 +0100

    fixing payment with stripe

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,10 +1,10 @@
 class UserMailer < ApplicationMailer
-  default from: 'phillip.spicker@gmx.de'
+  default from: 'your-email@example.com'
 
   def contact_form(email, name, message)
   @message = message
     mail(:from => email,
-      :to => 'your-email@example.com',
+      :to => 'phillip.spicker@gmx.de',
       :subject => "A new contact form message from #{name}")
   end
 
@@ -20,6 +20,14 @@ class UserMailer < ApplicationMailer
     mail( :to => user.email,
           :subject => "Welcome to #{@appname}!")
   end
+
+  def paid_order(user, product)
+    @user = user
+    @product = product
+    @appname = "Uperfit"
+    mail( :to => user.email,
+    :subject => "Your order on #{@appname}!")
+   end
 
 
 end
